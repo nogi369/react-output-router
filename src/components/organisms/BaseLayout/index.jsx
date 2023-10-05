b53a5b3db339b0e28281c5d8c794e8411c1942b8
nogi <Createc369.Re@gmail.com>
2023-10-05 09:35:58 +0900

    delete: AddTodo

--- src/components/organisms/BaseLayout/index.jsx
+++ src/components/organisms/BaseLayout/index.jsx
@@ -1,17 +1,15 @@
 import { Navigation } from "../../molecules/Navigation";
 import styles from "./styles.module.css";
 
-export const BaseLayout = ({ children, title }) => {
-  return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>{title}</h1>
-      <section className={styles.common}>
-        <Navigation />
-      </section>
-      {children}
-    </div>
-  );
-};
+export const BaseLayout = ({ children, title }) => (
+  <div className={styles.container}>
+    <h1 className={styles.title}>{title}</h1>
+    <section className={styles.common}>
+      <Navigation />
+    </section>
+    {children}
+  </div>
+);
 
 // {children}
 // https://choippo.com/react-component-children/
