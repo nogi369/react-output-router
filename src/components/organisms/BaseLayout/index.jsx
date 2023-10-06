7f1a700dbedbde977ce1107a727c3ac1fc21f2ae
nogi <Createc369.Re@gmail.com>
2023-10-06 16:54:43 +0900

    update: base layout

--- src/components/organisms/BaseLayout/index.jsx
+++ src/components/organisms/BaseLayout/index.jsx
@@ -3,10 +3,10 @@ import styles from "./styles.module.css";
 
 export const BaseLayout = ({ children, title }) => (
   <div className={styles.container}>
-    <h1 className={styles.title}>{title}</h1>
     <section className={styles.common}>
       <Navigation />
     </section>
+    <h1 className={styles.title}>{title}</h1>
     {children}
   </div>
 );
